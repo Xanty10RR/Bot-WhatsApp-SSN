b74e351c14a14ca4602842b42a83188378c44eba
Santiago <santiagocajamarca.@gmail.com>
2026-07-16 23:56:29 -0500

    fix: specify range for converting sheet to JSON in BBVA import

--- src/scripts/importBBVA.ts
+++ src/scripts/importBBVA.ts
@@ -5,7 +5,9 @@ const workbook = XLSX.readFile('./excels/BBVA.xlsx');
 
 const sheet = workbook.Sheets[workbook.SheetNames[0]];
 
-const data = XLSX.utils.sheet_to_json(sheet);
+const data = XLSX.utils.sheet_to_json(sheet, {
+    range: 1
+});
 
 console.log(`Total registros: ${data.length}`);
 console.log(data[0]);
