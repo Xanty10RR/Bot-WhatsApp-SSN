99eeb3a82a6e9e53725df04971b040f3a8344809
Xanty10RR <santiagocajamarca.@gmail.com>
2026-08-10 20:21:07 -0500

    feat(mainFlow): enhance welcome message and options for user interaction

--- src/templates/mainFlow.ts
+++ src/templates/mainFlow.ts
@@ -6,15 +6,15 @@ import { MENU_IDS } from "./constants";
 
 const mainFlow = addKeyword(['inicio','menu',EVENTS.WELCOME])
     .addAnswer (
-        '🌟 Hola, *¡Bienvenido a SuperGiros!* 🔵⚪\n',
+        '',
         {
             capture: false
         },
     async (ctx, { provider }) => {
         const list = {
-            header: { type: "text", text: "En que te podemos ayudar hoy" },
-            body: { text: "👋" },
-            footer: { text: "Por favor selecciona una opción:" },
+            header: { type: "text", text: "*¡Hola! 👋, Bienvenido al Asistente de SuperGiros* 🔵⚪\n\n" },
+            body: { text: "Estoy aquí las 24h para brindarte una mejor experiencia y ayudarte a consultar *códigos de convenios*, *instrucciones de pago sobre facturas de convenios* y *hacer requisiciones* de forma rápida." },
+            footer: { text: "¿Que deseas hacer hoy?, por favor selecciona una opción:" },
             action: {
                 button: "Lista de opciones",
                 sections: [
@@ -23,13 +23,13 @@ const mainFlow = addKeyword(['inicio','menu',EVENTS.WELCOME])
                         rows: [
                             {
                                 id: MENU_IDS.PRINCIPAL.OPCION1,
-                                title: "📋 Consultar convenio",
-                                description: "ℹ️ solicita informacion de convenio"
+                                title: "🔍 Consultar un convenio",
+                                description: "Solicita información de convenio, instrucciones de pago"
                             },
                             {
                                 id: MENU_IDS.PRINCIPAL.OPCION3,
-                                title: "Proceso de Requisicion",
-                                description: "Presenta dificultades en el sistema"
+                                title: "📋 Proceso de requisición",
+                                description: "Solicita activos, insumos, repuestos, servicios..."
                             }
                         ]
                     }
