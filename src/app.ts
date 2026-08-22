7628e7119b03df22469ebcb56c8cb02e05172eff
Xanty10RR <santiagocajamarca.@gmail.com>
2026-08-22 00:38:15 -0500

    refactor: streamline bot initialization by removing unused imports and simplifying setup..

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
-import { createBot, MemoryDB } from '@builderbot/bot';
-import { provider } from './provider';
 import { config } from './config';
-import templates from './templates';
-import './provider/database';
+import { getBot } from './bot';
 
 const main = async () => {
-    const { httpServer } = await createBot({
-        flow: templates,
-        provider: provider,
-        database: new MemoryDB(),
-    });
+    const { httpServer } = await getBot();
 
     const port = Number(config.PORT || 3001);
-    httpServer(port); // levanta el servidor interno de Builderbot
+
+    httpServer(port);
 
     console.log(`✅ Bot corriendo en http://localhost:${port}`);
 };
