295758fcf559916db8bc84c91036ddd780fc673e
Xanty10RR <santiagocajamarca.@gmail.com>
2026-08-22 01:21:43 -0500

    test definitivo webhook

--- api/webhook.ts
+++ api/webhook.ts
@@ -4,13 +4,15 @@ export default function handler(
     req: VercelRequest,
     res: VercelResponse
 ) {
-    console.log("🔥 WEBHOOK VERCEL:", req.method, req.url);
+    console.log("🔥🔥🔥 ESTOY EN api/webhook.ts 🔥🔥🔥");
+    console.log("METHOD:", req.method);
+    console.log("URL:", req.url);
+    console.log("QUERY:", req.query);
 
     return res.status(200).json({
-        ok: true,
-        source: "api/webhook.ts",
-        message: "ESTE ES EL WEBHOOK DE VERCEL",
-        method: req.method,
+        prueba: "FUNCIONA",
+        archivo: "api/webhook.ts",
+        metodo: req.method,
         query: req.query,
     });
 }
