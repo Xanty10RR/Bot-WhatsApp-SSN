bf9c105a7644febf82a8aa9649defc8464da159c
Xanty10RR <santiagocajamarca.@gmail.com>
2026-08-24 01:23:30 -0500

    fix: update image format from PNG to WEBP in submenu1 flow (prod)

--- src/templates/submenus/submenu1.ts
+++ src/templates/submenus/submenu1.ts
@@ -50,7 +50,7 @@ export const submenu1Flow: any = addKeyword(MENU_IDS.PRINCIPAL.OPCION1)
         const rutaImagen = resolve(
           __dirname,
           "images",
-          `${convenio.codigo_convenio}.png`,
+          `${convenio.codigo_convenio}.webp`,
         );
         if (existsSync(rutaImagen)) {
           await flowDynamic([
